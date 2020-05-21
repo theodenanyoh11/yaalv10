3634c86262ccdbe9444bba14d2e3f103e7001ade
theodenanyoh11 <theo@theodenanyoh.com>
2020-05-20 20:17:46 -0400

    added styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,40 @@
+// Theme
+
+export default {
+    font: {
+        main: "Muli, sans-serif",
+      },
+
+    colors: {
+        primary: "#OD3B66",
+        secondary: "#FF6F0B",
+        gray: "#737B7D",
+        white: "#ffffff",
+        light: "#f8f8f8",
+        },
+    
+    breakpoints: {
+        mobile: "only screen and (max-width: 50rem)",
+        tablet: "only screen and (max-width: 65rem)",
+    },
+    spacings: {
+        xxSmall: ".25rem",
+        xSmall: ".5rem",
+        small: "1rem",
+        medium: "2rem",
+        large: "3rem",
+    },
+
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link: "color 0.2s ease",
+    },
+
+    shadows: {
+        shadow1: "0px 5px 20px rgba(30,30,31,0.05)",
+    },
+
+}
+
+
+  
